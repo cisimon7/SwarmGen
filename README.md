93445e0ece52b6dd083952a74532081640b78db3
Simon Idoko <cisimon7@gmail.com>
2024-12-20 16:32:51 +0200

    readme update

--- README.md
+++ README.md
@@ -3,3 +3,39 @@
 This repository contains the source code to reproduce the experiments in our paper: "Swarm-Gen: Fast Generation of Diverse Feasible Swarm Behaviors" where we combine a generative model (CVAE/VQ-VAE) with a safty filter (SF) to learn to generate diverse swarm behavior trajectories given a fixed start and goal points.
 
 <img src="./utils/imgs/teaser.png" width="800"/>
+
+
+## Getting Started
+
+1. Clone this repository:
+
+```
+git clone https://github.com/cisimon7/SwarmGen.git
+cd SwarmGen
+```
+2. Create a conda environment and install the dependencies:
+
+```
+conda create -n venv python=3.8
+conda activate venv
+pip install -r requirements.txt
+```
+
+3. Create a directory `resources`
+
+4. Download the [weights](https://owncloud.ut.ee/owncloud/s/K7MqFwSjdBk78a9) for our trained Models and save to the `resources` directory
+
+5. Download the [train and test dataset](https://owncloud.ut.ee/owncloud/s/zHQgBDLndDJPbkc) and also save to the `resources` directory.
+
+6. You should be able to run any of the train and/or test norebooks. Note that for `num_agent=4` or `num_agent=8`, the variables `lwidth` and `lheight` should both be set to 5, while for `num_agent=16`, they are set to 10. This applies for all the notebooks.
+
+
+## Visualization
+Visualization of diverse trajectories generated using the VQ-VAE model for a given fixed start and goal points.
+
+<img src="./utils/imgs/scene_1.gif" width="300"/> <img src="./utils/imgs/scene_2.gif" width="300"/>
+<br>
+<img src="./utils/imgs/scene_3.gif" width="300"/> <img src="./utils/imgs/scene_4.gif" width="300"/>
+
+
+###
--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,4 @@
+import numpy as np
 from math import floor
 
 import plotly
@@ -39,6 +40,56 @@ def multiviz_trajs(traj_x, traj_y, traj_z, nrows=2, ncols=2, num_agents=16, heig
         xaxis=dict(showticklabels=False, title=""),
         yaxis=dict(showticklabels=False, title=""),
         zaxis=dict(showticklabels=False, title=""),
+    )
+    fig.update_xaxes(showline=True, mirror=True)
+    fig.update_yaxes(showline=True, mirror=True)
+    fig.show()
+
+
+
+def single_view(traj_x, traj_y, traj_z, opacity=1, nrows=2, ncols=2, num_agents=16, height=1_200, width=1_200):
+    cols = plotly.colors.qualitative.Pastel
+    cols1 = plotly.colors.qualitative.Dark24
+
+    fig = make_subplots(
+        rows=nrows, cols=1, horizontal_spacing=0.05, vertical_spacing=0.05,
+        specs=[[{'type': 'scatter3d'} for _ in range(1)] for _ in range(nrows)]
+    )
+
+    if type(opacity) == int or type(opacity) == float:
+        opacity = np.ones(num_agents) * opacity
+
+    for i in range(int(nrows*ncols)):
+        for j in range(num_agents):
+            fig.add_trace(
+                go.Scatter3d(x=traj_x[i, j], y=traj_y[i, j], z=traj_z[i, j], mode='lines', opacity=opacity[j], showlegend=False, line=dict(width=7, color=cols[(i+2)%10])),
+                row=1+floor(i/ncols), col=1#+(i%ncols)
+            )
+        fig.add_trace(
+            go.Scatter3d(x=traj_x[i, :num_agents, 0], y=traj_y[i, :num_agents, 0], z=traj_z[i, :num_agents, 0], mode='markers', showlegend=False, marker=dict(symbol="square", size=5, color=cols1[-1])),
+            row=1+floor(i/ncols), col=1#+(i%ncols)
+        )
+        fig.add_trace(
+            go.Scatter3d(x=traj_x[i, :num_agents, -1], y=traj_y[i, :num_agents, -1], z=traj_z[i, :num_agents, -1], mode='markers', showlegend=False, marker=dict(symbol="x", size=3, color=cols1[-2])),
+            row=1+floor(i/ncols), col=1#+(i%ncols)
+        )
+
+    fig.update_layout(
+        template="seaborn", 
+        paper_bgcolor="rgba(0, 0, 0, 0)",
+        height=height, width=width, 
+        margin=dict(l=20, r=20, t=20, b=20),
+    )
+    fig.update_scenes(
+        aspectratio=dict(x=1, y=1, z=1),
+        camera=dict(
+            # up=dict(x=0, y=0, z=1),
+            # center=dict(x=0, y=0, z=0),
+            eye=dict(x=1.25, y=-1.25, z=1.25)
+        ),
+        xaxis=dict(showticklabels=False, title="", tickfont=dict(family='Helvetica', size=14, weight="bold")),# range=[-1.5, 2]),
+        yaxis=dict(showticklabels=False, title="", tickfont=dict(family='Helvetica', size=14, weight="bold")),# range=[-0.5, 2]),
+        zaxis=dict(showticklabels=False, title="", tickfont=dict(family='Helvetica', size=14, weight="bold")),# range=[-2, 2]),
     )
     fig.update_xaxes(showline=True, mirror=True)
     fig.update_yaxes(showline=True, mirror=True)
